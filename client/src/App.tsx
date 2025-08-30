365374e58b194974b1cd0c6045b7906e90b30fe5
natan <henrix494@gmail.com>
2025-08-30 18:19:10 +0300

    added client side

---
+++ client/src/App.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+interface table {
+  id: string;
+  name: string;
+  description: string;
+  tasks: [
+    {
+      id: number;
+      title: string;
+      type: string;
+      description: string;
+      boardId: string;
+    }
+  ];
+}
+function App() {
+  const [id, setId] = useState<string>("");
+  const [tableData, setTableData] = useState<table>();
+  const start = async () => {
+    const data = await fetch("http://localhost:3000/api/boards");
+    const final = await data.json();
+    if (data.status === 200) {
+      localStorage.setItem("board", final.boardId);
+      setId(final.boardId);
+    }
+  };
+  const isBoard = localStorage.getItem("board");
+  const reDirect = () => {
+    if (isBoard) window.location.href = isBoard;
+  };
+  const handleRed = async () => {
+    if (!isBoard) {
+      await start();
+      if (id && window.location.pathname !== `/${isBoard}`) reDirect();
+    }
+  };
+  const getBoardDetails = async () => {
+    const data = await fetch(
+      `http://localhost:3000/api/boards${window.location.pathname}`
+    );
+    const final = await data.json();
+    setTableData(final);
+  };
+  useEffect(() => {
+    handleRed();
+    getBoardDetails();
+  }, [id]);
+  if (isBoard && window.location.pathname !== `/${isBoard}`) reDirect();
+  return (
+    <main className="flex justify-center flex-col  items-center pt-20">
+      <div>
+        <h1>{tableData?.name}</h1>
+        <h3>{tableData?.description}</h3>
+      </div>
+      <div>
+        <ul className="list-none w-full">
+          {tableData?.tasks.map((item) => (
+            <li className="list-none">{item.title}</li>
+          ))}
+        </ul>
+      </div>
+    </main>
+  );
+}
+
+export default App;
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,12 @@
 import express from "express";
 import boardRoute from "./routes/BoardRoute";
+import cors from "cors";
 const app = express();
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use(express.json());
 app.use("/api/boards", boardRoute);
 // Routes
